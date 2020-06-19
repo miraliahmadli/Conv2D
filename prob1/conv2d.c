55aab91e09b5a065eb6950208bf25397437e2f35
Mirali <miraliahmadli@gmail.com>
2020-06-19 16:26:59 +0000

    Fixed bugs

--- prob1/conv2d.c
+++ prob1/conv2d.c
@@ -17,30 +17,31 @@ Assume the stride is always 1. Use solely the primitive scalar operations (i.e.,
 
 */
 
-#include <cstdio>
 #include <stdio.h>
 #include <stdlib.h>
-#include <iostream>
-#include <algorithm>
 #include "assert.h"
 
-#include "preprocessing/im2col.h"
-#include "preprocessing/col2im.h"
-using namespace std;
+#include "../preprocessing/im2col.h"
+#include "../preprocessing/col2im.h"
 
 float *input, *kernel, *output;
 float *input_col, *output_col;
 int N, H, W, C;
 int KH, KW, OC, IC;
 
+
 //  reads binary data from given files to input and kernel arrays
 void read_data(const char *input_file, const char *kernel_file){
+    printf("Opening files: %s %s\n", input_file, kernel_file);
     FILE *in, *kern;
     in = fopen(input_file, "rb");
     kern = fopen(kernel_file, "rb");
+    printf("Opening SUCCESS\n");
 
     if (in == NULL || kern == NULL){
-        printf("Could not open the input bin files");
+        printf("Could not open the input bin files %s %s\n", input_file, kernel_file);
+        if (in == NULL) printf("input bin is NULL\n");
+        if (kern == NULL) printf("kernel bin is NULL\n");
         fclose(in);
         fclose(kern);
         exit(-1);
@@ -50,27 +51,31 @@ void read_data(const char *input_file, const char *kernel_file){
     size_t kernel_read = 0;
 
     //  read dimensions
+    printf("Reading input dimensions\n");
     input_read += fread(&N, 4, 1, in);
     input_read += fread(&H, 4, 1, in);
     input_read += fread(&W, 4, 1, in);
     input_read += fread(&C, 4, 1, in);
     if (input_read != 4){
-        printf("Could not read dimensions, read input elems = %d", input_read);
+        printf("Could not read dimensions, read input elems = %ld\n", input_read);
         fclose(in);
         fclose(kern);
         exit(3);
     }
+    printf("N = %d H = %d W = %d C = %d\n", N, H, W, C);
 
-    kernel_read += fread(&KH, 4, 1, in);
-    kernel_read += fread(&KW, 4, 1, in);
-    kernel_read += fread(&OC, 4, 1, in);
-    kernel_read += fread(&IC, 4, 1, in);
+    printf("Reading kernel dimensions\n");
+    kernel_read += fread(&KH, 4, 1, kern);
+    kernel_read += fread(&KW, 4, 1, kern);
+    kernel_read += fread(&OC, 4, 1, kern);
+    kernel_read += fread(&IC, 4, 1, kern);
     if (kernel_read != 4){
-        printf("Could not read dimensions, read kernel elems = %d", kernel_read);
+        printf("Could not read dimensions, read kernel elems = %ld\n", kernel_read);
         fclose(in);
         fclose(kern);
         exit(3);
     }
+    printf("KH = %d KW = %d OC = %d IC = %d\n", KH, KW, OC, IC);
 
     //  allocate arrays
     input = (float *)malloc(N*H*W*C*4);
@@ -79,9 +84,10 @@ void read_data(const char *input_file, const char *kernel_file){
     int pad = (KH - 1)/2;
 
     //  read input image
+    printf("Reading input image\n");
     input_read += fread(input, 4, N*H*W*C, in);
     if (input_read != N*H*W*C + 4){
-        printf("Could not write dimensions, written elems = %d", input_read);
+        printf("Could not write dimensions, written elems = %ld\n", input_read);
         fclose(in);
         fclose(kern);
         free(input);
@@ -90,14 +96,16 @@ void read_data(const char *input_file, const char *kernel_file){
     }
 
     // apply im2col algorithm
+    printf("Applying im2col\n");
     im2col_cpu(input, C, H, W, KH, 1, pad, input_col);
-    freae(input);
+    free(input);
 
     // read filters
+    printf("Reading kernel\n");
     kernel = (float *)malloc(KH*KW*OC*IC*4);
     kernel_read += fread(kernel, 4, KH*KW*OC*IC, kern);
     if (kernel_read != KH*KW*OC*IC + 4){
-        printf("Could not write dimensions, written elems = %d", kernel_read);
+        printf("Could not write dimensions, written elems = %ld\n", kernel_read);
         fclose(in);
         fclose(kern);
         free(input);
@@ -105,6 +113,7 @@ void read_data(const char *input_file, const char *kernel_file){
         exit(3);
     }
 
+    printf("Reading SUCCESS\n");
     //  close files
     fclose(in);
     fclose(kern);
@@ -115,7 +124,7 @@ void write_data(const char *output_file){
     FILE *out;
     out = fopen(output_file, "wb");
     if (out == NULL){
-        printf("Could not open the output bin file");
+        printf("Could not open the output bin file\n");
         exit(-1);
     }
 
@@ -128,7 +137,7 @@ void write_data(const char *output_file){
     elems_written += fwrite(&W, 4, 1, out);
     elems_written += fwrite(&OC, 4, 1, out);
     if (elems_written != 4){
-        printf("Could not write dimensions, written elems = %d", elems_written);
+        printf("Could not write dimensions, written elems = %ld\n", elems_written);
         fclose(out);
         exit(2);
     }
@@ -136,7 +145,7 @@ void write_data(const char *output_file){
     //  write output matrix
     elems_written += fwrite(output, 4, N*H*W*OC, out);
     if (elems_written != N*H*W*OC + 4){
-        printf("Could not write dimensions, written elems = %d", elems_written);
+        printf("Could not write dimensions, written elems = %ld\n", elems_written);
         fclose(out);
         exit(2);
     }
@@ -158,7 +167,7 @@ void conv2d(){
     int Z = N * H * W;
     int X = OC;
 
-    output_col = (float *) malloc(x * Y * sizeof(float));
+    output_col = (float *) malloc(X * Y * sizeof(float));
     for (int i = 0; i < X; i++){
         for (int j = 0; j < Z; j++){
             float sum = 0.0f;
@@ -178,10 +187,10 @@ void conv2d(){
     //  output = output_col.transpose(3, 1, 2, 0)
     //  outpu_col dimension: OC * H * W * N
     //  output will have dimension: N * H * W * OC
-    for(int oc = 0; i < OC; oc++){
+    for(int oc = 0; oc < OC; oc++){
         for(int h = 0; h < H; h++){
             for(int w = 0; w < W; w++){
-                for(int n = 0; n < N: n++){
+                for(int n = 0; n < N; n++){
                     output[n*H*W*OC + h*W*OC + w*OC + oc] = output_col[oc*Z + h*W*N + w*N + n];
                 }
             }
@@ -192,8 +201,9 @@ void conv2d(){
 
 
 int main(int argc, char *argv[]){
+    printf("Reading STARTS\n");
     //  read data from binary files
-    read_data(argv[0], argv[1]);
+    read_data(argv[1], argv[2]);
     //  preprocessing (this part can be handled in reading part)
 
     //  convolution operation
@@ -201,7 +211,7 @@ int main(int argc, char *argv[]){
     conv2d();
 
     //  write results to binary file
-    write_data(argv[2]);
+    write_data(argv[3]);
 
     return 0;
 }
